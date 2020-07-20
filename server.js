844b4b60cd2de1b90df1f12e231fb69d8a1a70d9
eduardosukeda <eduardosukeda@live.com>
2020-07-20 12:26:17 -0300

    Preparação deploy

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
-//Importa as dependências que acabamos de instalar
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
-// Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
-app.use(express.static(__dirname + '/dist/spa-fiap-lembretes'));
+app.use(express.static('./dist/spa-fiap-lembretes'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/spa-fiap-lembretes/index.html'));
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/spa-fiap-lembretes/'}),
+);
 
-// Inicia a aplicação pela porta configurada
 app.listen(process.env.PORT || 8080);
