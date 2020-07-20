5859bcb747b98f0ac9bb799de304ff108f061ae6
eduardosukeda <eduardosukeda@live.com>
2020-07-20 12:15:00 -0300

    Preparação deploy

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/spa-fiap-lembretes'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+res.sendFile(path.join(__dirname+'/dist/spa-fiap-lembretes/index.html'));
 });
 
 // Inicia a aplicação pela porta configurada
